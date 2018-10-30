76b651ef6d7f9e56d9f2c4cdf3826b8a6808fc9a
Eric Hunsberger <erichuns@gmail.com>
2018-10-30 12:24:48 -0400

    extract_sliding_windows uses view to save memory
    The data in `x` does not need to be copied.

--- conv2d.py
+++ conv2d.py
@@ -3,6 +3,16 @@ from __future__ import division
 import numpy as np
 
 
+def array_offset(x):
+    """Get offset of array data from base data in bytes."""
+    if x.base is None:
+        return 0
+
+    base_start = x.base.__array_interface__['data'][0]
+    start = x.__array_interface__['data'][0]
+    return start - base_start
+
+
 def calc_pad(pad, in_siz, out_siz, stride, ksize):
     """Calculate padding width.
 
@@ -94,15 +104,11 @@ def extract_sliding_windows_gradw(x,
             x, ((0, 0), (0, p2h), (0, p2w), (0, 0)),
             mode='constant',
             constant_values=(0.0, ))
-    x = x.reshape([n, int(x.shape[1] / sh), sh, int(x.shape[2] / sw), sw, c])
 
-    y = np.zeros([n, h2, w2, kh, kw, c])
-    for ii in range(h2):
-        for jj in range(w2):
-            h0 = int(np.floor(ii / sh))
-            w0 = int(np.floor(jj / sw))
-            y[:, ii, jj, :, :, :] = x[:, h0:h0 + kh, ii % sh, w0:w0 + kw, jj %
-                                      sw, :]
+    x_sn, x_sh, x_sw, x_sc = x.strides
+    y_strides = (x_sn, x_sh, x_sw, sh*x_sh, sw*x_sw, x_sc)
+    y = np.ndarray((n, h2, w2, kh, kw, c), dtype=x.dtype, buffer=x.data,
+                   offset=array_offset(x), strides=y_strides)
     return y
 
 
@@ -156,11 +162,11 @@ def extract_sliding_windows_gradx(x,
         x, ((0, 0), pph, ppw, (0, 0)),
         mode='constant',
         constant_values=(0.0, ))
-    y = np.zeros([n, h2, w2, kh, kw, c])
 
-    for ii in range(h2):
-        for jj in range(w2):
-            y[:, ii, jj, :, :, :] = x[:, ii:ii + kh, jj:jj + kw, :]
+    x_sn, x_sh, x_sw, x_sc = x.strides
+    y_strides = (x_sn, x_sh, x_sw, x_sh, x_sw, x_sc)
+    y = np.ndarray((n, h2, w2, kh, kw, c), dtype=x.dtype, buffer=x.data,
+                   offset=array_offset(x), strides=y_strides)
     return y
 
 
@@ -206,12 +212,10 @@ def extract_sliding_windows(x, ksize, pad, stride, floor_first=True):
         mode='constant',
         constant_values=(0.0, ))
 
-    y = np.zeros([n, h2, w2, kh, kw, c])
-    for ii in range(h2):
-        for jj in range(w2):
-            xx = ii * sh
-            yy = jj * sw
-            y[:, ii, jj, :, :, :] = x[:, xx:xx + kh, yy:yy + kw, :]
+    x_sn, x_sh, x_sw, x_sc = x.strides
+    y_strides = (x_sn, sh*x_sh, sw*x_sw, x_sh, x_sw, x_sc)
+    y = np.ndarray((n, h2, w2, kh, kw, c), dtype=x.dtype, buffer=x.data,
+                   offset=array_offset(x), strides=y_strides)
     return y
 
 
